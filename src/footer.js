3ab2d661be1cd11066330f5ecdaeb58257d0d675
Richard Jalonen <richard.jalonen@tptlive.ee>
2018-12-28 19:39:33 +0200

    added a footer, changed style

---
+++ src/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="app-footer">
+                <div>
+                    <span>&copy; Richard Jalonen </span>
+                    <br />
+                    <span>jalonen.richard@gmail.com</span>
+                </div>
+
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigator from './navbar';
 import Welcome from './welcome';
+import Footer from './footer';
 
 const navRoot = document.getElementById("navigation");
 const contentRoot = document.getElementById("pageContent");
+const footerRoot = document.getElementById("footer");
 
-ReactDOM.render( < Navigator / > , navRoot);
-ReactDOM.render( < Welcome / > , contentRoot);
+ReactDOM.render(< Navigator />, navRoot);
+ReactDOM.render(< Welcome />, contentRoot);
+ReactDOM.render(< Footer />, footerRoot)
